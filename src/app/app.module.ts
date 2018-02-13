c994c8232e92dcf3a5c6b22a84d7860b3d75d171
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-02-13 00:10:02 -0200

    data binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MeuFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,47 @@
+
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url:string = 'http://loiane.com';
+  cursoAngular:boolean = true;
+  urlImagem:string = "http://lorempixel.com/400/200/nature/";
+  valorAtual:string = "";
+  valorSalvo:string = "";
+  isMouseOver:boolean = false;
+
+
+  constructor() { }
+
+  getValor(){
+    return 1
+  }
+
+  botaoClicado()
+  {
+    alert("botao clicado");
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  ngOnInit() {
+  }
+
+}
